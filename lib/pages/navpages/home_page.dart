e1ced2288817854d1766411d9ae7a3c7e290fae7
Promss <tima.nurlanbekov@gmail.com>
2022-11-08 13:41:06 +0600

    added a carousel

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:silkroad_trip/resources/resources.dart';
+
+class CardItem {
+  final String imageName;
+  final String title;
+
+  const CardItem({
+    required this.imageName,
+    required this.title,
+  });
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,31 +21,96 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<CardItem> items = [
+    CardItem(
+      imageName: Images.articlesUnSel,
+      title: 'Статьи',
+    ),
+    CardItem(
+      imageName: Images.newsUnSel,
+      title: 'Новости',
+    ),
+    CardItem(
+      imageName: Images.lifehackUnSel,
+      title: 'Лайфхаки',
+    ),
+    CardItem(
+      imageName: Images.hotelUnSel,
+      title: 'Отели',
+    ),
+    CardItem(
+      imageName: Images.restaurantsUnSel,
+      title: 'Рестораны',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          padding: EdgeInsets.only(top: 10, left: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 40),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Text(
-                'SilkRoad Trip',
-                style: GoogleFonts.tenorSans(
-                    fontSize: 25,
-                    color: Color.fromRGBO(96, 125, 139, 1.0),
-                    fontWeight: FontWeight.w200),
+              Container(
+                padding: EdgeInsets.only(left: 20, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'SilkRoad Trip',
+                      style: GoogleFonts.tenorSans(
+                          fontSize: 25,
+                          color: Color.fromRGBO(96, 125, 139, 1.0),
+                          fontWeight: FontWeight.w200),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.search),
+                      iconSize: 30,
+                    )
+                  ],
+                ),
               ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.search),
-                iconSize: 30,
-              )
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                height: 80,
+                child: ListView.separated(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  separatorBuilder: (context, _) => SizedBox(
+                    width: 0,
+                    height: 0,
+                  ),
+                  itemBuilder: (context, index) =>
+                      buildCard(item: items[index]),
+                ),
+              ),
             ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
+
+Widget buildCard({required CardItem item}) => Container(
+      width: 90,
+      child: Column(
+        children: [
+          Expanded(
+              child: SvgPicture.asset(
+            item.imageName,
+            fit: BoxFit.cover,
+          )),
+          SizedBox(
+            height: 4,
+          ),
+          Text(item.title)
+        ],
+      ),
+    );
--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -17,4 +17,15 @@ class Images {
   static const String profileUnSel = 'assets/navBar/profileUnselected.svg';
   static const String searchSel = 'assets/navBar/searchSel.svg';
   static const String searchUnSel = 'assets/navBar/searchUnSel.svg';
+  static const String articlesSel = 'assets/homePage/articlesSel.svg';
+  static const String articlesUnSel = 'assets/homePage/articlesUnSel.svg';
+  static const String hotelSel = 'assets/homePage/hotelSel.svg';
+  static const String hotelUnSel = 'assets/homePage/hotelUnSel.svg';
+  static const String lifehackSel = 'assets/homePage/lifehackSel.svg';
+  static const String lifehackUnSel = 'assets/homePage/lifehackUnSel.svg';
+  static const String newsSel = 'assets/homePage/newsSel.svg';
+  static const String newsUnSel = 'assets/homePage/newsUnSel.svg';
+  static const String restaurantsSel = 'assets/homePage/restaurantsSel.svg';
+  static const String restaurantsUnSel = 'assets/homePage/restaurantsUnSel.svg';
+
 }
